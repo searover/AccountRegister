a5fa0a119fb8c3f1b31834e40d19df0c499df9d8
Baiguang <alex987965@gmail.com>
2015-07-24 11:32:13 +0800

    add account captcha service implementation add randomgenerator

--- AccountAggregator/AccountCaptcha/src/main/java/com/searover/mvnbook/account/captcha/AccountCaptchaException.java
+++ AccountAggregator/AccountCaptcha/src/main/java/com/searover/mvnbook/account/captcha/AccountCaptchaException.java
@@ -4,4 +4,15 @@ package com.searover.mvnbook.account.captcha;
  * Created by searover on 7/23/15.
  */
 public class AccountCaptchaException extends Exception {
+
+    public AccountCaptchaException() {
+    }
+
+    public AccountCaptchaException(String message) {
+        super(message);
+    }
+
+    public AccountCaptchaException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
---
+++ AccountAggregator/AccountCaptcha/src/main/java/com/searover/mvnbook/account/captcha/AccountCaptchaServiceImpl.java
@@ -0,0 +1,88 @@
+package com.searover.mvnbook.account.captcha;
+
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import com.google.code.kaptcha.util.Config;
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * Created by searover on 7/24/15.
+ */
+public class AccountCaptchaServiceImpl implements AccountCaptchaService, InitializingBean{
+
+    private DefaultKaptcha producer;
+
+    private Map<String, String> captchaMap = new HashMap<String, String>();
+
+    private List<String> preDefinedTexts;
+
+    private int textCount = 0;
+
+    public String generateCaptchaKey() throws AccountCaptchaException {
+        String key = RandomGenerator.getRandomString();
+        String value = getCaptchaText();
+        captchaMap.put(key, value);
+        return key;
+    }
+
+    private String getCaptchaText() {
+        if(preDefinedTexts != null && !preDefinedTexts.isEmpty()){
+            String text = preDefinedTexts.get(textCount);
+            textCount = (textCount + 1) % preDefinedTexts.size();
+            return text;
+        }else {
+            return producer.createText();
+        }
+    }
+
+    public byte[] generateCaptchaImage(String captchaKey) throws AccountCaptchaException, IOException {
+       String text = captchaMap.get(captchaKey);
+        if(text == null){
+            throw new AccountCaptchaException();
+        }
+        BufferedImage image = producer.createImage(text);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        try {
+            ImageIO.write(image, "jpg", out);
+
+        } catch (IOException e) {
+            throw new AccountCaptchaException("Failed to write captcha stream!", e);
+        }
+        return out.toByteArray();
+    }
+
+    public boolean validateCaptcha(String captchaKey, String captchaValue) throws AccountCaptchaException {
+        String text = captchaMap.get(captchaKey);
+        if(text == null){
+            throw new AccountCaptchaException("Captcha key '" + captchaKey + "' not found!");
+        }
+        if(text.equals(captchaValue)){
+            captchaMap.remove(captchaKey);
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public List<String> getPreDefinedTexts() {
+        return preDefinedTexts;
+    }
+
+    public void setPreDefinedTexts(List<String> preDefinedTexts) {
+        this.preDefinedTexts = preDefinedTexts;
+    }
+
+    public void afterPropertiesSet() throws Exception {
+        producer = new DefaultKaptcha();
+
+        producer.setConfig(new Config(new Properties()));
+    }
+}
